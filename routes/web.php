6ef1656ae68efc27847c76f9a18850324d66a020
Bao Anh Le <anh.lb194470@gmail.com>
2022-12-02 12:28:34 +0700

    Post,User,Category Model Updated

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\PostController;
+use App\Http\Controllers\Admin\UserController;
 
 
 Route::get('/', function () {
@@ -13,6 +14,8 @@ Route::get('/', function () {
 //Routes
 Route::prefix('admin')->middleware('auth', 'isAdmin')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index']);
+
+    //For Categories
     Route::get('/category', [CategoryController::class, 'index']);
     Route::get('/add-category', [CategoryController::class, 'create']);
     Route::post('/add-category', [CategoryController::class, 'store']);
@@ -20,12 +23,18 @@ Route::prefix('admin')->middleware('auth', 'isAdmin')->group(function () {
     Route::put('/update-category/{category_id}', [CategoryController::class, 'update']);
     Route::get('/delete-category/{category_id}', [CategoryController::class, 'destroy']);
 
-    Route::get('posts', [PostController::class, 'index']);
+    //For Posts
+    Route::get('/posts', [PostController::class, 'index']);
     Route::get('/add-post', [PostController::class, 'create']);
     Route::post('/add-post', [PostController::class, 'store']);
     Route::get('/post/{post_id}', [PostController::class, 'edit']);
     Route::put('/update-post/{post_id}', [PostController::class, 'update']);
     Route::get('/delete-post/{post_id}', [PostController::class, 'destroy']);
+
+    //For Users
+    Route::get('/users', [UserController::class, 'index']);
+    Route::get('/user/{user_id}', [UserController::class, 'edit']);
+    Route::post('/update-user/{user_id}', [UserController::class, 'update']);
 });
 
 Auth::routes();
